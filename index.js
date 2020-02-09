49a1864477d25fec1df3a71078a952583fcfb088
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-09 15:49:31 +0500

    Инициализирует проект

---
+++ index.js
@@ -0,0 +1,65 @@
+const yargs = require(`yargs`);
+const pkg = require(`./package.json`);
+const notes = require(`./notes.js`);
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: `add`,
+  describe: `Добавить новую заметку`,
+  builder: {
+    title: {
+      type: `string`,
+      demandOption: true,
+      describe: `Название заметки`
+    },
+    text: {
+      type: `string`,
+      demandOption: true,
+      describe: `Текст заметки`
+    }
+  },
+  handler({title, text}) {
+    notes.addNote(title, text);
+  }
+});
+
+yargs.command({
+  command: `list`,
+  describe: `Показать список заметок`,
+  handler() {
+    notes.listNotes();
+  }
+});
+
+yargs.command({
+  command: `read`,
+  describe: `Показать контент выбранной заметки`,
+  builder: {
+    title: {
+      type: `string`,
+      demandOption: true,
+      describe: `Название заметки`
+    }
+  },
+  handler({title}) {
+    notes.readNote(title);
+  }
+});
+
+yargs.command({
+  command: `delete`,
+  describe: `Удалить заметку`,
+  builder: {
+    title: {
+      type: `string`,
+      demandOption: true,
+      describe: `Название заметки`
+    }
+  },
+  handler({title}) {
+    notes.deleteNote(title);
+  }
+});
+
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,76 @@
+const chalk = require(`chalk`);
+const fs = require(`fs`);
+const path = require(`path`);
+const notePath = path.join(__dirname, `notes.json`);
+
+const getNotes = (cb) => {
+  fs.readFile(notePath, `utf-8`, (err, content) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    try {
+      cb(JSON.parse(content));
+    } catch(e) {
+      cb([]);
+    }
+  });
+};
+
+const saveNotes = (content) => {
+  fs.writeFile(notePath, JSON.stringify(content), (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+};
+
+const addNote = (title, text) => {
+  getNotes((notes) => {
+    const dublicateNote = notes.find(note => note.title === title);
+    if (dublicateNote) {
+      console.log(chalk.red.inverse(`Заметка с таким названием уже существует`));
+    } else {
+      notes.push({title, text});
+      saveNotes(notes);
+      console.log(chalk.green.inverse(`Заметка добавлена`));
+    }
+  });
+};
+
+const listNotes = () => {
+  getNotes((notes) => {
+    if (notes.length) {
+      console.log(chalk.orange(`Ваши заметки:`));
+      notes.forEach((note) => {
+        console.log(note.title);
+      });
+    } else {
+      console.log(chalk.blue(`Заметок пока нет, добавьте первую`));
+    }
+  });
+};
+
+const readNote = (title) => {
+  getNotes((notes) => {
+    const note = notes.find((n) => n.title === title);
+    if (note) {
+      console.log(chalk.orange(`Заметка:`));
+      console.log(note.title);
+      console.log(note.text);
+    } else {
+      console.log(chalk.red(`Заметка с названием "${note.title}" не найдена`));
+    }
+  });
+};
+
+const deleteNote = (title) => {
+  
+};
+
+module.exports = {
+  addNote,
+  listNotes,
+  readNote,
+  deleteNote
+};
